132c22322a420fd9d068611537fe9b9b3e9eb606
KHALEDKHLYAN <khaledkhlyan@gmail.com>
2023-07-16 05:11:12 +0300

    image color

--- lib/main.dart
+++ lib/main.dart
@@ -59,24 +59,18 @@ class _HomeWidgetState extends State<HomeWidget> {
       backgroundColor: Colors.black54,
       appBar: AppBar(
         title: const Text('Flutter Demo'),
+        backgroundColor: Colors.pinkAccent,
       ),
       body: const Padding(
         padding: EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            Text(
-              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer non justo eu ligula consectetur sagittis. Aliquam at metus eget mi feugiat consectetur. Ut vel blandit ante. Proin faucibus diam quis tortor tempus, non aliquet dolor venenatis. Sed pretium venenatis lectus, eu scelerisque mauris pulvinar a. Nam laoreet dui a ultricies commodo. Proin at orci ut nulla porttitor tempor ut vel quam.',
-              style: TextStyle(
-                color: Colors.black87,
-                fontSize: 15.0,
-                fontStyle: FontStyle.normal,
-                backgroundColor: Colors.amberAccent,
-              ),
-            ),
-            Expanded(
-              child: Center(),
-            )
-          ],
+        child: Text(
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer non justo eu ligula consectetur sagittis. Aliquam at metus eget mi feugiat consectetur. Ut vel blandit ante. Proin faucibus diam quis tortor tempus, non aliquet dolor venenatis. Sed pretium venenatis lectus, eu scelerisque mauris pulvinar a. Nam laoreet dui a ultricies commodo. Proin at orci ut nulla porttitor tempor ut vel quam.',
+          style: TextStyle(
+            color: Colors.black87,
+            fontSize: 15.0,
+            fontStyle: FontStyle.normal,
+            backgroundColor: Colors.amberAccent,
+          ),
         ),
       ),
     );
